aa9c6f3b946aae04b4fca419907922370739bc7d
Ermia <khbzermia@gmail.com>
2025-10-15 21:46:45 +0330

    Add files via upload

---
+++ Js/footer.js
@@ -0,0 +1,10 @@
+
+    function changePic1(){
+    document.getElementById('bigImage').src='images/product/big/1.jpg';
+}
+    function changePic2(){
+    document.getElementById('bigImage').src='images/product/big/2.jpg';
+}
+    function changePic3(){
+    document.getElementById('bigImage').src='images/product/big/3.jpg';
+}
